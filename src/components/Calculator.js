ab80bd20a7067692516846a1e9c1a639d4cddc4a
tahseenzahra <tahseen1zahra@gmail.com>
2022-10-28 15:56:32 +0500

    Change div of character to button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,26 +5,26 @@ class Calculator extends React.PureComponent {
   render() {
     return (
       <section className="grid">
-        <div className="bgLGrey" id="a">0</div>
-        <div className="bgLGrey" id="b">AC</div>
-        <div className="bgLGrey" id="c">+/-</div>
-        <div className="bgLGrey" id="d">%</div>
-        <div className="bgOrange" id="e">/</div>
-        <div className="bgLGrey" id="f">7</div>
-        <div className="bgLGrey" id="g">8</div>
-        <div className="bgLGrey" id="h">9</div>
-        <div className="bgOrange" id="i">*</div>
-        <div className="bgLGrey" id="j">4</div>
-        <div className="bgLGrey" id="k">5</div>
-        <div className="bgLGrey" id="l">6</div>
-        <div className="bgOrange" id="m">-</div>
-        <div className="bgLGrey" id="n">1</div>
-        <div className="bgLGrey" id="o">2</div>
-        <div className="bgLGrey" id="p">3</div>
-        <div className="bgOrange" id="q">+</div>
-        <div className="bgLGrey" id="r">0</div>
-        <div className="bgLGrey" id="s">.</div>
-        <div className="bgOrange" id="t">=</div>
+        <button type="button" className="bgLGrey" id="a">0</button>
+        <button type="button" className="bgLGrey" id="b">AC</button>
+        <button type="button" className="bgLGrey" id="c">+/-</button>
+        <button type="button" className="bgLGrey" id="d">%</button>
+        <button type="button" className="bgOrange" id="e">/</button>
+        <button type="button" className="bgLGrey" id="f">7</button>
+        <button type="button" className="bgLGrey" id="g">8</button>
+        <button type="button" className="bgLGrey" id="h">9</button>
+        <button type="button" className="bgOrange" id="i">*</button>
+        <button type="button" className="bgLGrey" id="j">4</button>
+        <button type="button" className="bgLGrey" id="k">5</button>
+        <button type="button" className="bgLGrey" id="l">6</button>
+        <button type="button" className="bgOrange" id="m">-</button>
+        <button type="button" className="bgLGrey" id="n">1</button>
+        <button type="button" className="bgLGrey" id="o">2</button>
+        <button type="button" className="bgLGrey" id="p">3</button>
+        <button type="button" className="bgOrange" id="q">+</button>
+        <button type="button" className="bgLGrey" id="r">0</button>
+        <button type="button" className="bgLGrey" id="s">.</button>
+        <button type="button" className="bgOrange" id="t">=</button>
       </section>
     );
   }

4af1bc1ae616ee8c4d5e268fc6c8fa18c5e6c638
tahseenzahra <tahseen1zahra@gmail.com>
2022-11-03 22:07:22 +0500

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import Calculator from './components/Calculator';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from './components/Navbar';
 
 class App extends React.PureComponent {
   render() {
     return (
-      <React.StrictMode>
-        <div className="App">
-          <header className="AppHeader">
-            <Calculator />
-          </header>
+      <Router>
+        <div>
+          <div>
+            <Navbar />
+          </div>
+          <Switch>
+          <Route path="/">
+              <Home />
+            </Route>
+            <Route exact path="/calculator">
+              <Calculator />
+            </Route>
+            <Route exact path="/quote">
+              <Quote />
+            </Route>
+          </Switch>
         </div>
-      </React.StrictMode>
+      </Router>
+      // <React.StrictMode>
+      //   <div className="App">
+      //     <header className="AppHeader">
+      //       <Calculator />
+      //     </header>
+      //   </div>
+      // </React.StrictMode>
     );
   }
 }
